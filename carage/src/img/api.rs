72d23a4a94fcc081c10b4740d5fa4cadd1c57dee
JoseFilipeFerreira <jose.filipe.matos.ferreira@gmail.com>
2021-06-14 22:51:27 +0100

    add image server

---
+++ carage/src/img/api.rs
@@ -0,0 +1,52 @@
+use crate::fairings::{Claims, Db};
+use lazy_static::lazy_static;
+use rocket::{
+    data::{Data, Limits, ToByteUnit},
+    fs::NamedFile,
+};
+use std::{
+    fs, io,
+    path::{Path, PathBuf},
+};
+use uuid::Uuid;
+
+lazy_static! {
+    pub static ref ROUTES: Vec<rocket::Route> = routes![get, create, remove];
+}
+
+#[post("/<id>")]
+pub async fn get(conn: Db, id: String) -> Option<NamedFile> {
+    // TODO: get path from db via id
+    let path = id;
+    NamedFile::open(Path::new("images/").join(path)).await.ok()
+}
+
+#[post("/create/<filename>", data = "<img>")]
+pub async fn create(
+    conn: Db,
+    filename: PathBuf,
+    img: Data,
+    _claims: Claims,
+    limits: &Limits,
+) -> Result<String, std::io::Error> {
+    let id = Uuid::new_v4();
+    let path = Path::new("images/").join(id.to_string()).join(
+        filename
+            .as_path()
+            .extension()
+            .ok_or_else(|| io::ErrorKind::InvalidInput)?,
+    );
+    //TODO: insert id and path into db
+    img.open(limits.get("image").unwrap_or(10.mebibytes()))
+        .into_file(path)
+        .await?;
+    Ok(id.to_string())
+}
+
+#[delete("/remove/<id>")]
+pub async fn remove(conn: Db, id: String, _claims: Claims) -> io::Result<()> {
+    // TODO: get path from db via id
+    let path = id;
+    //TODO: delete entry from database
+    fs::remove_file(Path::new("images/").join(path))
+}
--- carage/src/main.rs
+++ carage/src/main.rs
@@ -7,6 +7,7 @@ extern crate rocket_sync_db_pools;
 pub mod ad;
 pub mod car;
 pub mod fairings;
+pub mod img;
 pub mod schema;
 pub mod user;
 use crate::ad::{api::ROUTES as AD_ROUTES, fav_ad::api::ROUTES as FAVAD_ROUTES};
@@ -14,6 +15,7 @@ use crate::car::{
     api::ROUTES as CAR_ROUTES, maintenance::api::ROUTES as MAINT_ROUTES,
     model::api::ROUTES as MODEL_ROUTES, share::api::ROUTES as SHARE_ROUTES,
 };
+use crate::img::api::ROUTES as IMG_ROUTES;
 use crate::user::api::ROUTES as USER_ROUTES;
 
 #[launch]
@@ -27,6 +29,7 @@ pub fn rocket() -> _ {
         .mount("/car/model", MODEL_ROUTES.to_vec())
         .mount("/car/maintenance", MAINT_ROUTES.to_vec())
         .mount("/car/share", SHARE_ROUTES.to_vec())
+        .mount("/img", IMG_ROUTES.to_vec())
         .attach(fairings::Cors())
         .attach(fairings::Db::fairing())
 }
