8a1c762abf3a1eecb71cb9dfb67ad1210ca2fb45
jtexeira <38398296+jtexeira@users.noreply.github.com>
2021-06-14 17:15:19 +0100

    User mod fixes (#24)

--- carage/src/user/api.rs
+++ carage/src/user/api.rs
@@ -17,10 +17,13 @@ pub async fn create(conn: Db, user: Json<ApiUser>) -> Option<Json<DbUser>> {
 }
 
 //TODO: Error reporting
-#[post("/", data = "<email>")]
-pub async fn get(conn: Db, email: String) -> Option<Json<User>> {
-    match conn.run(|c| User::get(email, c)).await {
-        Ok(u) => Some(Json(u)),
+#[get("/")]
+pub async fn get(conn: Db, claims: Claims) -> Option<Json<User>> {
+    match conn.run(|c| User::get(claims.email, c)).await {
+        Ok(mut u) => {
+            u.passwd = "[REDACTED]".to_owned();
+            Some(Json(u))
+        }
         _ => None,
     }
 }
@@ -35,7 +38,7 @@ pub async fn remove(conn: Db, claims: Claims) -> Option<Json<DbUser>> {
 
 #[post("/login", data = "<creds>")]
 pub async fn login(conn: Db, creds: Json<UserCreds>) -> Option<Json<String>> {
-    let clone_passwd = creds.password.clone();
+    let clone_passwd = creds.passwd.clone();
     let clone_email = creds.email.clone();
     if let Ok(user) = conn.run(move |c| User::get(creds.email.clone(), &c)).await {
         if user.check_passwd(&clone_passwd) {
@@ -44,13 +47,14 @@ pub async fn login(conn: Db, creds: Json<UserCreds>) -> Option<Json<String>> {
                 encode(
                     &Header::default(),
                     &claims,
-                    &EncodingKey::from_secret("secret".as_ref()),
+                    &EncodingKey::from_secret(b"secret".as_ref()),
                 )
                 .unwrap(),
             ))
         } else {
             None
-        };
+        }
+    } else {
+        None
     }
-    None
 }
