c645eaf2b70a181bf77f01177de25c75b1942ebe
jtexeira <38398296+jtexeira@users.noreply.github.com>
2021-06-19 15:10:32 +0100

    Add auth to file endpoints (#72)

--- carage/src/img/api.rs
+++ carage/src/img/api.rs
@@ -1,5 +1,8 @@
 use super::{File, FileApi};
-use crate::fairings::{Claims, Db};
+use crate::{
+    car::Car,
+    fairings::{Claims, Db},
+};
 use lazy_static::lazy_static;
 use rocket::serde::json::Json;
 use rocket::{
@@ -19,7 +22,6 @@ lazy_static! {
 
 #[get("/<id>")]
 pub async fn get(conn: Db, id: String) -> Option<NamedFile> {
-    // TODO: get path from db via id
     let id = Uuid::parse_str(&id).unwrap();
     let path = match conn.run(move |c| File::get(id, c)).await {
         Ok(f) => Some(f.id),
@@ -34,41 +36,61 @@ pub async fn get(conn: Db, id: String) -> Option<NamedFile> {
 pub async fn create(
     conn: Db,
     img: Json<FileApi>,
-    _claims: Claims,
+    claims: Claims,
     limits: &Limits,
 ) -> Result<Json<File>, std::io::Error> {
-    let id = Uuid::new_v4();
-    let path = Path::new("images/").join(id.to_string());
-    let bytes = image_base64::from_base64(img.image.clone());
+    let limits = limits.clone();
+    conn.run(move |c| {
+        let car = Car::get(&img.car_id, c).unwrap();
+        if car.owner == claims.email {
+            let id = Uuid::new_v4();
+            let path = Path::new("images/").join(id.to_string());
+            let bytes = image_base64::from_base64(img.image.clone());
 
-    if bytes.len() > limits.get("file/image").unwrap_or_else(|| 10.mebibytes()) {
-        Err(io::Error::new(io::ErrorKind::InvalidInput, "Image too big"))
-    } else {
-        let mut image_file = fs::File::create(path)?;
-        image_file.write_all(&bytes)
-    }?;
+            if bytes.len() > limits.get("file/image").unwrap_or_else(|| 10.mebibytes()) {
+                Err(io::Error::new(io::ErrorKind::InvalidInput, "Image too big"))
+            } else {
+                let mut image_file = fs::File::create(path)?;
+                image_file.write_all(&bytes)
+            }?;
 
-    let db_entry = File {
-        id,
-        filename: img.filename.clone(),
-        car_id: img.car_id.clone(),
-    };
-    conn.run(move |c| db_entry.insert(c))
-        .await
-        .map(Json)
-        .map_err(|_| io::Error::new(io::ErrorKind::InvalidInput, "Invalid base64 format"))
+            let db_entry = File {
+                id,
+                filename: img.filename.clone(),
+                car_id: img.car_id.clone(),
+            };
+            db_entry
+                .insert(c)
+                .map_err(|_| io::Error::new(io::ErrorKind::InvalidInput, "Invalid base64 format"))
+        } else {
+            Err(io::Error::new(io::ErrorKind::InvalidInput, "Image too big"))
+        }
+    })
+    .await
+    .map(Json)
+    .map_err(|_| io::Error::new(io::ErrorKind::InvalidInput, "Invalid base64 format"))
 }
 
 #[delete("/remove/<img>")]
-pub async fn remove(conn: Db, _claims: Claims, img: String) -> Option<Json<File>> {
-    if fs::remove_file(Path::new("images/").join(img.clone())).is_ok() {
-        if let Ok(img) = Uuid::parse_str(&img) {
-            match conn.run(move |c| File::delete(img, c)).await {
-                Ok(u) => Some(Json(u)),
-                _ => None,
-            }
-        } else {
-            None
+pub async fn remove(conn: Db, claims: Claims, img: String) -> Option<Json<File>> {
+    if let Ok(id) = Uuid::parse_str(&img) {
+        match conn
+            .run(move |c| {
+                let file = File::get(id, c)?;
+                let car = Car::get(&file.car_id, c)?;
+                if car.owner == claims.email {
+                    let f = File::delete(id, c);
+                    fs::remove_file(Path::new("images/").join(img.clone()))
+                        .map_err(|_| diesel::result::Error::NotFound)?;
+                    f
+                } else {
+                    Err(diesel::result::Error::NotFound)
+                }
+            })
+            .await
+        {
+            Ok(u) => Some(Json(u)),
+            _ => None,
         }
     } else {
         None
